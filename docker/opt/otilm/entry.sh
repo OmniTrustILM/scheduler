a0c0db93d5072a02fdbbbc69f329ef10e7094c0b
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-06-15 11:38:41 +0200

    Resolve copilot comments

--- docker/opt/otilm/entry.sh
+++ docker/opt/otilm/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 otilmHome="/opt/otilm"
-source ${otilmHome}/static-functions
+. ${otilmHome}/static-functions
 
 log "INFO" "Launching the Scheduler"
 exec java $JAVA_OPTS -jar ./app.jar
