66868b473af4c338611dd3c55be4e5396c5238ee
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-06-15 10:59:32 +0200

    Update job group name to 'otilm'

--- src/main/java/com/otilm/scheduler/constants/JobConstants.java
+++ src/main/java/com/otilm/scheduler/constants/JobConstants.java
@@ -4,7 +4,7 @@ public class JobConstants {
 
     public static final String CLASS_TOBE_EXECUTED = "JOBCLASS";
 
-    public static final String GROUP_NAME = "czertainly";
+    public static final String GROUP_NAME = "otilm";
 
     public static final String JOB_TRIGGER_SUFFIX = "_trigger";
 
